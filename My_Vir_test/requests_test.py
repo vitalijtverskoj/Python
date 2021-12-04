5330cddb2f505b80e9beb2dbc9ee6df554fc95d5
vitalijtverskoj <94721997+vitalijtverskoj@users.noreply.github.com>
2021-12-04 19:52:02 +0300

    Delete requests_test.py

--- My_Vir_test/requests_test.py
+++
@@ -1,7 +0,0 @@
-from requests import get, utils
-
-
-response = get('http://geekbrains.ru')
-encodings = utils.get_encoding_from_headers(response.headers)
-content = response.content.decode(encoding=encodings)
-print(content)
