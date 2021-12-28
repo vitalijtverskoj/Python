7f82ac2aaf4e477a8e71d7c79c7933f4e7e13b1c
vitalijtverskoj <vitalijtverskoj@gmail.com>
2021-12-28 20:37:26 +0300

    lesson 11

---
+++ vasilev_victor_les_11_1.py
@@ -0,0 +1,21 @@
+class Date:
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+
+    @classmethod  # должен извлекать число, месяц, год и преобразовывыть в int
+    def cm(cls, data):
+        day, month, year = [int(n) for n in data.split('-')]
+        return cls(day, month, year)
+
+    @staticmethod  # должен проводить валидацию числа, месяца и года
+    def sm(obj):
+        if 0 < obj.day <= 31 and 0 < obj.month <= 12 and 0 <= obj.year:
+            return f'день - {obj.day}, месяц - {obj.month}, год - {obj.year}'
+        else:
+            return 'Некорректная дата'
+
+
+data_1 = Date.cm('12-13-95')
+print(Date.sm(data_1))
